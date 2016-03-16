82994757965a31a9a2e7d3981fedbf1681506bff
Rajesh Mulapulli <murajesh@gmail.com>
2016-03-15 22:20:40 -0500

    Initial commit

---
+++ src/ca/demo/test1/Main.java
@@ -0,0 +1,19 @@
+package ca.demo.test1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data = new SalesData();
+		
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hello World!");
+		System.out.println("Good Morning.");
+	}
+
+}
---
+++ src/ca/demo/test1/SalesData.java
@@ -0,0 +1,9 @@
+package ca.demo.test1;
+
+public class SalesData {
+	int data[] = {0, 4, 2};
+	
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+}
